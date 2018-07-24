2d64eecff90f279c41a2a6f90fa0fa567a7decbd
Dominic Joel  Minaya Colón <d.minaya@sisalril.gov.do>
2018-07-24 11:54:13 -0400

    Lista el api de Compras

---
+++ WebApiFacturacion/Models/ComprasProcesadas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiFacturacion.Models
+{
+    public class ComprasProcesadas
+    {
+        public int Id_Compra { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public decimal Itebis { get; set; }
+        public decimal PrecioTotal { get; set; }
+
+    }
+}
